16d22318033e2a2fced9a57db1ee74e19660c242
AngelDown12 <angeldown12xz@gmail.com>
2025-07-31 18:44:27 -0600

    Crear Kick.js

---
+++ Plugins/Grupos/Kick.js
@@ -0,0 +1,12 @@
+const handler = async (m, { conn }) =>
+  m.mentionedJid?.[0] || m.quoted?.sender
+    ? (await conn.groupParticipantsUpdate(m.chat, [m.mentionedJid[0] || m.quoted.sender], 'remove'),
+       m.reply('☁️ Intruso eliminado.'))
+    : m.reply('☁️ _Menciona al usuario que deseas eliminar._');
+
+handler.command = ['kick', 'prepusio'] 
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+export default handler;
